9e1318be8c22c95b519d3372e47dea196ca49e85
ricjonsu-impact360 <ricjon.su@impact360design.com>
2025-06-23 14:57:20 +0800

    create login and register api

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use('/api/users', require('./routes/user.routes.js'));
+
 app.get('/', (req, res) => {
     res.send('API is running...');
 });
